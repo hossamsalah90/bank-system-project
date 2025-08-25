5810c6bd72c76e4a92de8ef159c9f51921afac47
hossam salah <hossamofficer@gmail.com>
2025-08-25 21:35:32 +0300

    Create validation.h
    add valiation.h

---
+++ validation.h
@@ -0,0 +1,29 @@
+#ifndef VALIDATION_H
+#define VALIDATION_H
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+class validation
+{
+    public:
+    static bool validname(string name){
+       if(name.length()<5||name.length()>20){
+    for(int i=0;i<name.length();i++){
+        if(!isalpha(name[i]))
+           return false;
+    }
+       }
+    return true;
+    }
+    static bool validpassword(string password){
+    if(password.length()<8||password.length()>20)
+    {return false;}
+    return true;
+    }
+};
+
+
+
+#endif // VALIDATION_H
